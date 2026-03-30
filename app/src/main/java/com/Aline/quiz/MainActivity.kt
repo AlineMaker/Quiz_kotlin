875cda4a9144ca26cb35f6b59b05dd7a441d1efd
ALINE ALVES DE SOUZA <25132561@dsn127m1242314.cfp127aluno.local>
2026-03-30 10:59:46 -0300

    Tela Quiz

--- app/src/main/java/com/Aline/quiz/MainActivity.kt
+++ app/src/main/java/com/Aline/quiz/MainActivity.kt
@@ -4,30 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.HorizontalAlignmentLine
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.Aline.quiz.components.CardScreen
-import com.Aline.quiz.components.QuizScreen
 import com.Aline.quiz.screens.start.StartScreen
+import com.Aline.quiz.screens.start.quiz.QuizScreen
 import com.Aline.quiz.ui.theme.QuizTheme
-import org.intellij.lang.annotations.JdkConstants
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,7 +20,7 @@ class MainActivity : ComponentActivity() {
             QuizTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
 
-                    StartScreen  (
+                    QuizScreen  (
                         modifier = Modifier.padding(innerPadding)
                     )
 
---
+++ app/src/main/java/com/Aline/quiz/screens/start/quiz/QuizScreen.kt
@@ -0,0 +1,120 @@
+package com.Aline.quiz.screens.start.quiz
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.Aline.quiz.R
+import com.Aline.quiz.components.CardScreen
+
+@Composable
+fun QuizScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(Color(254, 147, 211, 255)),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        // verticalArrangement = Arrangement.Center
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+        Image(
+            modifier = Modifier.height(90.dp),
+            painter = painterResource(R.drawable.quiz),
+            contentDescription = ""
+        )
+
+        Spacer(modifier = Modifier.height(100.dp))
+        Row(
+            modifier = Modifier
+                .fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.Center
+
+
+        ) {
+            Card(
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(70.dp),
+                border = BorderStroke(1.dp, Color.Black),
+                colors = CardDefaults.cardColors(containerColor = Color(67, 196, 164, 255)),
+                elevation = CardDefaults.cardElevation(4.dp)
+
+            ) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(
+                        text = "Perguntas de 1 a 3",
+                        fontSize = (32.sp)
+                    )
+                    Card(
+                        modifier = Modifier
+                            .width(300.dp)
+                            .height(70.dp),
+                        border = BorderStroke(1.dp, Color.Black),
+                        colors = CardDefaults.cardColors(containerColor = Color(67, 196, 164, 255)),
+                        elevation = CardDefaults.cardElevation(4.dp)
+
+                    ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            contentAlignment = Alignment.Center
+                        ) {
+                            Text(
+                                text = "Perguntas de 1 a 3",
+                                fontSize = (32.sp)
+                            )
+                        }
+                        }
+                }
+            }
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Card(
+            modifier = Modifier
+                .fillMaxWidth(0.90f)
+                .height(500.dp),
+            border = BorderStroke(1.dp, Color.Black),
+            colors = CardDefaults.cardColors(containerColor = Color(255, 255, 255, 255)),
+            elevation = CardDefaults.cardElevation(4.dp)
+
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "",
+                    fontSize = (32.sp)
+                )
+            }
+        }
+    }
+}
